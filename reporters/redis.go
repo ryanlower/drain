a92bdb0cebe8f8383da48b05feb397cfbca4e7d4
Ryan Lower <rpjlower@gmail.com>
2014-12-20 16:51:55 -0800

    Do redis AUTH if REDIS_PASSWORD env is set

--- reporters/redis.go
+++ reporters/redis.go
@@ -3,6 +3,7 @@ package reporters
 import (
 	"encoding/json"
 	"log"
+	"os"
 
 	"github.com/garyburd/redigo/redis"
 	"github.com/ryanlower/drain/parser"
@@ -11,6 +12,8 @@ import (
 // Redis is a reporter that publishes hit statues to redis
 // The address of the redis server to be published can be
 // controlled using the REDIS_ADDRESS env (defaults to "localhost:6379")
+// Setting the optional REDIS_PASSWORD env will cause the client to AUTH
+// using that password on connection
 type Redis struct {
 	Reporter
 }
@@ -29,11 +32,15 @@ func (r *Redis) Report(hit *parser.ParsedLogLine) {
 
 func connect() redis.Conn {
 	address := envOrDefault("REDIS_ADDRESS", "localhost:6379")
-	// TODO, auth handling
 	conn, err := redis.Dial("tcp", address)
 	// TODO, how to handle errors in reporters?
 	if err != nil {
 		log.Panic(err)
+	}
+
+	// AUTH if REDIS_PASSWORD env is set
+	if password := os.Getenv("REDIS_PASSWORD"); password != "" {
+		conn.Do("AUTH", password)
 	}
 
 	return conn
